3f3c3d49ebfc7ed7237b288d3ea1472000779002
joseg1037 <74805028+joseg1037@users.noreply.github.com>
2023-05-17 18:52:32 -0600

    Update FRONT-END-LOGIK.js

--- FRONT-END-LOGIK.js
+++ FRONT-END-LOGIK.js
@@ -1,67 +1,79 @@
+const _=window._;
 let [hours, minutes, seconds, milliseconds]=[0,0,0,0];
-let timeoutId;
+let timeoutId, timer;
 let divHoursValue,
 divMinutesValue,
 divSecondsValue, 
-divMilisecondsValue, 
+divMillisecondsValue, 
 startButton, 
 divItem,
 divItemRounds;
+
 
 window.onload=function(){
 	divHoursValue=document.getElementById('div-hours-value');
 	divMinutesValue=document.getElementById('div-minutes-value');
 	divSecondsValue=document.getElementById('div-seconds-value'); 
-	divMilisecondsValue=document.getElementById('div-milliseconds-value');
+	divMillisecondsValue=document.getElementById('div-milliseconds-value');
 	startButton=document.getElementById("start-button");
 	divItem=document.getElementById("div-item");	
 	divItemRounds=document.getElementById("div-round-items");
+	startButton.addEventListener("click", function(){
+		GetMilliseconds();
+		StartTimer();
+	});
 }
-
-function StartTimer(){	
-	milliseconds+=1;
-	if(milliseconds==100){
-		seconds++;
-		milliseconds=0;
-		if(seconds==60){
-			minutes++;
-			seconds=0;
-			if(minutes==60){
-				hours++;				
-				minutes=0;
-			}
+const debouncedStartTimer=_.debounce(StartTimer, 1000);	
+const debouncedMilliseconds=_.debounce(GetMilliseconds, 1);	
+function StartTimer(){		
+	seconds++;
+	if(seconds===60){
+		seconds=0;
+		minutes++;
+		if(minutes===60){
+			minutes=0;
+			hours++;				
 		}
-	}
+	}	
 	let h = hours;
 	let m = minutes;
 	let s = seconds;
-	let ms = milliseconds;
 	if(hours<10)
 		h="0"+h;
 	if(minutes<10)
 		m="0"+m;
 	if(seconds<10)
-		s="0"+s;
-	if(milliseconds<10)
-		ms="0"+ms;
+		s="0"+s;	
 	divHoursValue.innerHTML=h+":";
 	divMinutesValue.innerHTML=m+":";
 	divSecondsValue.innerHTML=s+":";
-	divMilisecondsValue.innerHTML=ms;
-	timeoutId=setTimeout(StartTimer, 10);
+	debouncedStartTimer(StartTimer, 1000);
 	startButton.disabled=true;
 }
+
+function GetMilliseconds(){
+	milliseconds++;	
+	if(milliseconds===100)
+		milliseconds=0;		
+	let ms=milliseconds;
+	if(milliseconds<10)
+		ms="0"+ms;
+	divMillisecondsValue.innerHTML=ms;
+	debouncedMilliseconds(GetMilliseconds, 1);
+}
 function StopTimer(){
-	clearTimeout(timeoutId);
-	startButton.disabled=false;
+	debouncedMilliseconds.cancel();
+	debouncedStartTimer.cancel();
+	startButton.disabled=false;	
 }
 function ClearTimer(){
-	clearTimeout(timeoutId);
+	debouncedMilliseconds.cancel();
+	debouncedStartTimer.cancel();	
 	hours=0, minutes=0, seconds=0, milliseconds=0;
 	divHoursValue.innerHTML="00:";
 	divMinutesValue.innerHTML="00:";
 	divSecondsValue.innerHTML="00:";
-	divMilisecondsValue.innerHTML="00";
+	divMillisecondsValue.innerHTML="00";	
 	startButton.disabled=false;
 }
 function AddRound(){
